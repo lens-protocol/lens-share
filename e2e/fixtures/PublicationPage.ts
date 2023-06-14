3be81fd08512c743f6e642521c682190fa22c9f3
Cesare Naldi <naldi.cesare@gmail.com>
2023-06-14 20:01:21 +0200

    Write test covering 'Just once' use case

--- e2e/fixtures/PublicationPage.ts
+++ e2e/fixtures/PublicationPage.ts
@@ -3,7 +3,7 @@ export class PublicationPage {
   readonly options: Locator;
   readonly attribution: Locator;
 
-  constructor(private readonly page: Page, private readonly publicationId: string) {
+  constructor(private readonly page: Page, readonly publicationId: string) {
     this.options = page.locator("ul > li > label");
     this.attribution = page.getByTestId("attribution");
   }
@@ -15,4 +15,16 @@ export class PublicationPage {
   async openAsSharedBy(appId: string) {
     await this.page.goto(`/p/${this.publicationId}?by=${appId}`);
   }
+
+  async waitRedirect() {}
+
+  async justOnce(label: string) {
+    await this.page.getByLabel(label).click();
+    await this.page.getByRole("button", { name: "Just once" }).click();
+    const currentUrl = await this.page.url();
+    await this.page.waitForURL((newUrl) => newUrl.toString() !== currentUrl);
+    return this.page.url();
+  }
+
+  async always(appId: string) {}
 }
