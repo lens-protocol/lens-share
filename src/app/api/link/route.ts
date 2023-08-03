8315b918fd52a07a1cbbc63da5c66c82b320786e
Wassim <wawa.h8@gmail.com>
2023-08-03 13:00:34 +0100

    Introducing link generation

---
+++ src/app/api/link/route.ts
@@ -0,0 +1,62 @@
+import { NextResponse } from "next/server";
+
+import { AppManifest } from "@/data";
+import { fetchAllApps } from "@/data/storage";
+
+
+export async function POST(req: Request) {
+  const res = await req.json();
+
+  if (!res.link) {
+    return NextResponse.json({ error: "Missing handle in the request body" });
+  }
+
+  const apps = await fetchAllApps();
+
+  const linkType = determineLinkTypeForAppManifests(res.link as string, apps as AppManifest[]);
+
+  return NextResponse.json({ linkType });
+}
+
+const SHARE_PROFILE_URL = "https://share.lens.xyz/u/";
+const SHARE_PUBLICATION_URL = "https://share.lens.xyz/p/";
+
+function determineLinkTypeForApp(
+  link: string,
+  manifest: AppManifest
+): { type: "profile" | "publication" | "unknown"; url?: string } {
+  const profileUrl = manifest.routes.profile?.url;
+  const publicationUrl = manifest.routes.publication?.url;
+
+  if (profileUrl) {
+    const profileRegex = new RegExp(`^${profileUrl.replace(":handle", "(.+)")}$`);
+
+    const profileMatch = profileRegex.exec(link);
+    if (profileMatch) {
+      const customProfileUrl = SHARE_PROFILE_URL + profileMatch[1];
+      return { type: "profile", url: customProfileUrl };
+    }
+  }
+
+  if (publicationUrl) {
+    const publicationIdRegex = new RegExp(`^${publicationUrl.replace(":id", "(.+)")}$`);
+
+    const publicationMatch = publicationIdRegex.exec(link);
+    if (publicationMatch) {
+      const customPublicationUrl = SHARE_PUBLICATION_URL + publicationMatch[1];
+      return { type: "publication", url: customPublicationUrl };
+    }
+  }
+
+  return { type: "unknown" };
+}
+
+function determineLinkTypeForAppManifests(link: string, appManifestsArray: AppManifest[]) {
+  for (const manifest of appManifestsArray) {
+    const linkInfo = determineLinkTypeForApp(link, manifest);
+    if (linkInfo.type !== "unknown") {
+      return linkInfo;
+    }
+  }
+  return { type: "unknown" };
+}
--- src/components/LinkGenerator.tsx
+++ src/components/LinkGenerator.tsx
@@ -1,57 +1,108 @@
+"use client";
+
 import { CheckIcon, ClipboardDocumentIcon } from "@heroicons/react/24/solid";
+import { useState } from "react";
+import { toast } from "react-hot-toast";
 
 import { getLink } from "@/app/actions";
 
+import Spinner from "./Spinner";
+
+
 /* eslint-disable @next/next/no-img-element */
+type LinkData = { linkType: { type: "profile" | "publication" | "unknown"; url?: string } };
 
 export function LinkGenerator({}) {
+  const [link, setLink] = useState("");
+  const [newLink, setNewLink] = useState<string | undefined>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const onChangeLink = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLink(e.target.value);
+  };
+
+  const generateLink = async () => {
+    try {
+      const response = await fetch("/api/link", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ link }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to generate link");
+      }
+
+      const data: LinkData = await response.json();
+
+      if (data.linkType.type === "unknown") {
+        toast.error("Link not supported");
+        return;
+      }
+
+      setNewLink(data.linkType.url ?? undefined);
+    } catch (error) {
+      console.error("Error generating link:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <form action={getLink}>
-      <div className="flex flex-col justify-between items-center gap-8 sm:gap-10">
-        <div className="w-full text-left">
-          <p className="header-six !font-bold text-lightForest">Create a Lens Share link</p>
-        </div>
-
-        <div className="flex flex-col w-full items-start gap-2 font-gintoNord font-medium text-[16px]">
-          <p className="text-lightForest">Original link</p>
-
-          <label className="w-full relative h-[58px]">
-            <input
-              className="absolute top-0 left-0 w-full bg-white/10 h-[58px] rounded-xl text-white placeholder:text-white/60 pl-4 pr-8"
-              placeholder="Add a post/profile link from any Lens app..."
-              // value={link}
-              name="link"
-              // onChange={onChangeLink}
-              // formAction={getLink}
-            />
-
-            {/* // Verify that the link is correct and a Lens link */}
-            {/* {link ? (
-              <CheckIcon className="text-lightForest h-6 w-6 absolute right-4 top-4" />
-            ) : null} */}
-          </label>
-        </div>
-
-        {/* // Verify that the link is correct and a Lens link */}
-        {/* {link ? (
-          <div className="flex flex-col w-full items-start gap-2 font-gintoNord font-medium text-[16px]">
-            <p className="text-lightForest">Copy and share your universal Lens Share link</p>
-
-            <button className="w-full flex items-center justify-between h-[58px] bg-white/10  rounded-xl px-4 hover:bg-white/20 hover:outline outline-lightForest">
-              <p className="text-white">{link}</p>
-
-              <ClipboardDocumentIcon className="text-lightForest h-6 w-6" />
-            </button>
-          </div>
-        ) : null} */}
-
-        <button
-          type="submit"
-          className="bg-lightForest text-darkForest font-gintoNord w-full p-3 rounded-xl font-medium uppercase"
-        >
-          Generate link
-        </button>
+    <div className="flex flex-col justify-between items-center gap-8 sm:gap-10">
+      <div className="w-full text-left">
+        <p className="header-six !font-bold text-lightForest">Create a Lens Share link</p>
       </div>
-    </form>
+
+      <div className="flex flex-col w-full items-start gap-2 font-gintoNord font-medium text-[16px]">
+        <p className="text-lightForest">Original link</p>
+
+        <label className="w-full relative h-[58px]">
+          <input
+            className="absolute top-0 left-0 w-full bg-white/10 h-[58px] rounded-xl text-white placeholder:text-white/60 pl-4 pr-8"
+            placeholder="Add a post/profile link from any Lens app..."
+            value={link}
+            onChange={onChangeLink}
+          />
+
+          {newLink ? (
+            <CheckIcon className="text-lightForest h-6 w-6 absolute right-4 top-4" />
+          ) : null}
+
+          {isLoading ? (
+            <div className="absolute right-4 top-5">
+              <Spinner />
+            </div>
+          ) : null}
+        </label>
+      </div>
+
+      {newLink ? (
+        <div className="flex flex-col w-full items-start gap-2 font-gintoNord font-medium text-[16px]">
+          <p className="text-lightForest">Copy and share your universal Lens Share link</p>
+
+          <button
+            onClick={() => {
+              toast.success("Copied to clipboard");
+              navigator.clipboard.writeText(newLink);
+            }}
+            className="w-full flex items-center justify-between h-[58px] bg-white/10  rounded-xl px-4 hover:bg-white/20 hover:outline outline-lightForest"
+          >
+            <p className="text-white">{newLink}</p>
+
+            <ClipboardDocumentIcon className="text-lightForest h-6 w-6" />
+          </button>
+        </div>
+      ) : null}
+
+      <button
+        onClick={generateLink}
+        className="bg-lightForest text-darkForest font-gintoNord w-full p-3 rounded-xl font-medium uppercase hover:opacity-95"
+      >
+        Generate link
+      </button>
+    </div>
   );
 }
